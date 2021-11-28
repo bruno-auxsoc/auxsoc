547a080f3a43df0bfc9d3a205d674b90fb711909
Bruno Moura <contato@sitesistema.com>
2021-11-28 15:49:08 -0300

    correção dashboard

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -3,24 +3,32 @@
 namespace App\Controllers;
 
 use App\Models\AgendamentoModel;
+use App\Models\AtendimentoModel;
 use App\Models\FamiliaModel;
+use App\Models\VisitaModel;
 
 class Dashboard extends BaseController
 {
     protected $familiaModel;
     protected $agendamentoModel;
+    protected $atendimentoModel;
+    protected $visitaModel;
     
 
     public function __construct()
     {
         $this->familiaModel = new FamiliaModel();
         $this->agendamentoModel = new AgendamentoModel();
+        $this->atendimentoModel = new AtendimentoModel();
+        $this->visitaModel = new VisitaModel();
     }
 
     public function index()
     {
         $familias = $this->familiaModel->findAll();
         $agendamentos = $this->agendamentoModel->findAll();
+        $atendimentos = $this->atendimentoModel->findAll();
+        $visitas = $this->visitaModel->findAll();
 
 
         //  para testar dados vindo do bd
@@ -28,7 +36,9 @@ class Dashboard extends BaseController
 
         $dados = [
             'familias' => $familias,
-            'agendamentos' => $agendamentos
+            'agendamentos' => $agendamentos,
+            'atendimentos' => $atendimentos,
+            'visitas' => $visitas
         ];
         return view('dashboard/index', $dados);
     }
